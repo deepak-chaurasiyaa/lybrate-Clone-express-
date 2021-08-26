a4843e0e339a1bf397aecdc6d8230d62548025cb
ankur598 <ankursingh.as278@gmail.com>
2021-08-26 20:03:25 +0530

    ask a question

--- lybrate.js
+++ lybrate.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const app = new express();
+//const mongoose = require("mongoose");
+const path = require('path');
+const app = express();
 const connect = require("./src/configs/db");
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: false }))
+app.use("/static", express.static(path.join(__dirname, "views")));
+app.set("view engine", "ejs")
+
+//questions page link
+const userQuestion = require("./src/askAquestion/controllers/controllers")
+app.use("/questions", userQuestion);
 
 app.listen(3333, async function (req, res) {
   await connect();
