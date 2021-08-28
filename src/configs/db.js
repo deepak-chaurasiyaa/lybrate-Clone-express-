dac09a2e91ceeb9f22f217d48ae1721adfe187fc
Sagar Biswas <mrsagar105@gmail.com>
2021-08-28 18:48:22 +0530

    updated db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,4 @@
 const mongoose = require("mongoose");
-// const connect = () => {
-//   return mongoose.connect("mongodb://127.0.0.1:27017/web10mongoose", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   });
-// };
 const connect = () => {
   return mongoose.connect(
     "mongodb+srv://ughcqqtp:G.9F2@QtzzQqHGv@cluster0.ketra.mongodb.net/Lybrate?retryWrites=true&w=majority",
